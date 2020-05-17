b9d67d248544f01a805b3451a6d33f9fa326c4c8
Alexander Bleissem <alexander@bleissem.com>
2020-05-17 23:47:41 +0200

    initial commit

---
+++ src/BuildHelper.Versioning/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Build.Utilities;
+using System;
+
+namespace BuildHelper.Versioning
+{
+    public class Class1 : Task
+    {
+        public override bool Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
